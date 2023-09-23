1af1452265d13d4e462a99a299976d4289f405c5
zaina-fannana <126976872+zaina-fannana@users.noreply.github.com>
2023-09-23 23:03:19 +0300

    step two

--- src/UserPage/creator/MyOpportunities/ID/edit/step-one/EditStepOnePage.js
+++ src/UserPage/creator/MyOpportunities/ID/edit/step-one/EditStepOnePage.js
@@ -5,18 +5,20 @@ import { PlusOutlined } from "@ant-design/icons";
 import { Button, Form } from "antd";
 import { AiOutlineClose } from "react-icons/ai";
 import "./EditStepOnePage.css";
+import StepTwoEdit from "../step-two/StepTwoEdit";
 
 const EditStepOnePage = () => {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [errors, setErrors] = useState({});
-
   const [formData, setFormData] = useState({
     title: "",
     company: "",
     productionPersonnel: [],
     productionDescription: "",
   });
+
+  const [showStepTwo, setShowStepTwo] = useState(false);
 
   useEffect(() => {
     if (data) {
@@ -81,166 +83,180 @@ const EditStepOnePage = () => {
     return Object.values(errors).some((error) => error);
   };
 
+  const handleContinue = () => {
+    window.history.pushState(
+      {},
+      "",
+      "creator/opportunities/1833/edit/step-two"
+    );
+    setShowStepTwo(true);
+  };
+
   return (
     <div className="my-12" style={{ minHeight: "70vh", background: "#f5f5f5" }}>
       <p className="StepOne">Posting an Opportunity</p>
-      <div
-        className="flex flex-col mx-auto gap-3"
-        style={{
-          textAlign: "end",
-          maxWidth: "600px",
-        }}
-      >
-        <div>
-          <TextField
-            value={formData.title}
-            onChange={handleInputChange}
-            name="title"
-            label="Title of Production"
-            variant="standard"
-            placeholder="Robinhoud"
-            style={{ width: "100%" }}
-          />
-          <p className="text-sm text-red-500 p-2 inline-block">
-            {errors.title && errors.title}
-          </p>
-        </div>
+      {showStepTwo ? (
+        <StepTwoEdit />
+      ) : (
+        <div
+          className="flex flex-col mx-auto gap-3"
+          style={{
+            textAlign: "end",
+            maxWidth: "600px",
+            marginRight: "365px",
+          }}
+        >
+          <div>
+            <TextField
+              value={formData.title}
+              onChange={handleInputChange}
+              name="title"
+              label="Title of Production"
+              variant="standard"
+              placeholder="Robinhoud"
+              style={{ width: "100%" }}
+            />
+            <p className="text-sm text-red-500 p-2 inline-block">
+              {errors.title && errors.title}
+            </p>
+          </div>
 
-        <div>
-          <TextField
-            value={formData.company}
-            onChange={handleInputChange}
-            name="company"
-            label="Production Company"
-            variant="standard"
-            placeholder="Company Name"
-            style={{ width: "100%" }}
-          />
-          <p className="text-sm text-red-500 p-2 inline-block">
-            {errors.company && errors.company}
-          </p>
-        </div>
+          <div>
+            <TextField
+              value={formData.company}
+              onChange={handleInputChange}
+              name="company"
+              label="Production Company"
+              variant="standard"
+              placeholder="Company Name"
+              style={{ width: "100%" }}
+            />
+            <p className="text-sm text-red-500 p-2 inline-block">
+              {errors.company && errors.company}
+            </p>
+          </div>
 
-        <div>
-          <Form name="dynamic_form_item" style={{ maxWidth: 600 }}>
-            <Form.List
-              name="names"
-              rules={[
-                {
-                  validator: async (_, names) => {
-                    if (!names || names.length < 1) {
-                      return Promise.reject(
-                        new Error("At least 1 passenger is required")
-                      );
-                    }
+          <div>
+            <Form name="dynamic_form_item" style={{ maxWidth: 600 }}>
+              <Form.List
+                name="names"
+                rules={[
+                  {
+                    validator: async (_, names) => {
+                      if (!names || names.length < 1) {
+                        return Promise.reject(
+                          new Error("At least 1 passenger is required")
+                        );
+                      }
+                    },
                   },
-                },
-              ]}
-            >
-              {(fields, { add, remove }, { errors }) => (
-                <>
-                  {fields.map((field, index) => (
-                    <Form.Item required={false} key={field.key}>
-                      <div className="flex items-center">
-                        <Form.Item
-                          {...field}
-                          validateTrigger={["onChange"]}
-                          rules={[
-                            {
-                              required: true,
-                              whitespace: true,
-                              message:
-                                "Please input passenger's name or delete this field.",
-                            },
-                          ]}
-                          noStyle
-                        >
-                          <TextField
-                            name={`productionPersonnel[${index}]`}
-                            label="Production Personnel"
-                            variant="standard"
-                            placeholder="Production Personnel"
-                            style={{ width: "100%" }}
-                            value={formData.productionPersonnel[index] || ""}
-                            onChange={(e) => handle(e, index)}
-                          />
-                        </Form.Item>
-                        {fields.length > 0 ? (
-                          <AiOutlineClose
-                            className="cursor-pointer dynamic-delete-button"
-                            onClick={() => remove(field.name)}
-                          />
-                        ) : null}
-                      </div>
+                ]}
+              >
+                {(fields, { add, remove }, { errors }) => (
+                  <>
+                    {fields.map((field, index) => (
+                      <Form.Item required={false} key={field.key}>
+                        <div className="flex items-center">
+                          <Form.Item
+                            {...field}
+                            validateTrigger={["onChange"]}
+                            rules={[
+                              {
+                                required: true,
+                                whitespace: true,
+                                message:
+                                  "Please input passenger's name or delete this field.",
+                              },
+                            ]}
+                            noStyle
+                          >
+                            <TextField
+                              name={`productionPersonnel[${index}]`}
+                              label="Production Personnel"
+                              variant="standard"
+                              placeholder="Production Personnel"
+                              style={{ width: "100%" }}
+                              value={formData.productionPersonnel[index] || ""}
+                              onChange={(e) => handle(e, index)}
+                            />
+                          </Form.Item>
+                          {fields.length > 0 ? (
+                            <AiOutlineClose
+                              className="cursor-pointer dynamic-delete-button"
+                              onClick={() => remove(field.name)}
+                            />
+                          ) : null}
+                        </div>
+                      </Form.Item>
+                    ))}
+                    <Form.Item>
+                      <Button
+                        onClick={() => add()}
+                        className="ant-btn ant-btn-default border-blue-600 css-dev-only-do-not-override-nnuwmp mui-rtl-dwgqy6 sm:w-[40%]"
+                      >
+                        <p className="flex items-center gap-2 text-blue-600 font-semibold">
+                          Production Personal
+                          <PlusOutlined />
+                        </p>
+                      </Button>
+
+                      <Form.ErrorList errors={errors} />
                     </Form.Item>
-                  ))}
-                  <Form.Item>
-                    <Button
-                      onClick={() => add()}
-                      className="ant-btn ant-btn-default border-blue-600 css-dev-only-do-not-override-nnuwmp mui-rtl-dwgqy6 sm:w-[40%]"
-                    >
-                      <p className="flex items-center gap-2 text-blue-600 font-semibold">
-                        Production Personal
-                        <PlusOutlined />
-                      </p>
-                    </Button>
+                  </>
+                )}
+              </Form.List>
+            </Form>
+            <p className="text-sm text-red-500 p-2 inline-block">
+              {errors.productionPersonnel && errors.productionPersonnel}
+            </p>
+          </div>
 
-                    <Form.ErrorList errors={errors} />
-                  </Form.Item>
-                </>
-              )}
-            </Form.List>
-          </Form>
-          <p className="text-sm text-red-500 p-2 inline-block">
-            {errors.productionPersonnel && errors.productionPersonnel}
-          </p>
-        </div>
+          <div>
+            <TextField
+              value={formData.productionDescription}
+              onChange={handleInputChange}
+              name="productionDescription"
+              minRows={7}
+              label="Production Company"
+              style={{ width: "100%" }}
+            />
+            <p className="text-sm text-red-500 p-2 inline-block">
+              {errors.productionDescription && errors.productionDescription}
+            </p>
+          </div>
 
-        <div>
-          <TextField
-            value={formData.productionDescription}
-            onChange={handleInputChange}
-            name="productionDescription"
-            minRows={7}
-            label="Production Company"
-            style={{ width: "100%" }}
-          />
-          <p className="text-sm text-red-500 p-2 inline-block">
-            {errors.productionDescription && errors.productionDescription}
-          </p>
-        </div>
-
-        <div className="flex items-center gap-4">
-          <button className="save" href={"/creator"}>
-            Save For Later
-          </button>
-
-          <> ︎ ︎ ︎</>
-
-          {formData.title &&
-          formData.company &&
-          formData.productionDescription !== "" &&
-          isLoading ? (
-            <div className="border-2 border-blue-700 bg-blue-700 rounded-md text-lg text-white px-4 py-1 font-semibold hover:bg-blue-600 duration-200"></div>
-          ) : (
-            <button
-              onClick={handleSubmit}
-              className="border-2 border-blue-700 bg-blue-700 rounded-md text-lg text-white px-4 py-1 font-semibold hover:bg-blue-600 duration-200"
-            >
-              {formData.title &&
-              formData.company &&
-              formData.productionDescription !== "" ? (
-                <div> </div>
-              ) : (
-                "Continue"
-              )}
+          <div className="flex items-center gap-4">
+            <button className="save" href={"/creator"}>
+              Save For Later
             </button>
-          )}
-          <p className="text-sm text-red-500 p-2 inline-block">
-            {hasErrors() && "This field is required."}
-          </p>
+
+            <> ︎ ︎ ︎</>
+
+            {formData.title &&
+            formData.company &&
+            formData.productionDescription !== "" &&
+            isLoading ? (
+              <div className="border-2 border-blue-700 bg-blue-700 rounded-md text-lg text-white px-4 py-1 font-semibold hover:bg-blue-600 duration-200"></div>
+            ) : (
+              <button
+                onClick={handleContinue}
+                className="border-2 border-blue-700 bg-blue-700 rounded-md text-lg text-white px-4 py-1 font-semibold hover:bg-blue-600 duration-200"
+              >
+                {formData.title &&
+                formData.company &&
+                formData.productionDescription !== "" ? (
+                  <div> </div>
+                ) : (
+                  "Continue"
+                )}
+              </button>
+            )}
+            <p className="text-sm text-red-500 p-2 inline-block">
+              {hasErrors() && "This field is required."}
+            </p>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
--- src/UserPage/creator/MyOpportunities/ID/edit/step-two/StepTwoEdit.js
+++ src/UserPage/creator/MyOpportunities/ID/edit/step-two/StepTwoEdit.js
@@ -1,9 +1,261 @@
 import React, { useEffect, useState } from "react";
+import dayjs from "dayjs";
+import axios from "axios";
+import CloseIcon from "@mui/icons-material/Close";
+import { Switch, TextField, Button } from "@mui/material";
+import { IoAddOutline } from "react-icons/io5";
+import Tooltip from "@mui/material/Tooltip";
+import ClearIcon from "@mui/icons-material/Clear";
+import "./StepTwoEdit.css";
+import ModeEditOutlineIcon from "@mui/icons-material/ModeEditOutline";
 
+const StepTwoEdit = () => {
+  const currentDate = dayjs();
+  const [myRoles, setMyRoles] = useState([]);
+  const [expirationDate, setExpirationDate] = useState(null);
 
-const StepTwoEdit= () => {
-    return(
-        <h1>..</h1>
-    )
-}
+  const handleDateChange = (date) => {
+    setExpirationDate(date);
+  };
+  const handleBackToStepOne = () => {
+    window.history.pushState(
+      {},
+      "",
+      "creator/opportunities/1833/edit/step-one"
+    );
+  };
+  const handleSubmit = (event) => {
+    const token = localStorage.getItem("token");
+    event.preventDefault();
+
+    // schema
+    //   .validate(expirationDate, { abortEarly: false })
+    //   .then(() => {
+    //     console.log("Form data is valid:", expirationDate);
+    //   })
+    //   .catch((validationErrors) => {
+    //     console.log(validationErrors);
+    //   });
+
+    // (async () => {
+    //   try {
+    //     const response = await axios.put(
+    //       `http://casting-ec2-1307338951.us-east-2.elb.amazonaws.com:7001/opportunities/`,
+    //       { expirationDate: expirationDate },
+    //       {
+    //         headers: {
+    //           Authorization: `Bearer ${token}`,
+    //         },
+    //       }
+    //     );
+    //     if (response) {
+    //       setIsLoading(true);
+    //     }
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    // })();
+  };
+
+  const handleDynamicRoute = (e) => {
+    const token = localStorage.getItem("token");
+    e.preventDefault();
+
+    (async () => {
+      try {
+        const response = await axios.post(
+          `http://casting-ec2-1307338951.us-east-2.elb.amazonaws.com:7001/opportunities/roles`,
+          {
+            name: "",
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        if (response.data && response.data.id) {
+          // .push(
+          //   `/creator/opportunities/${.id}/roles/${response.data.id}/edit/step-one`
+          // );
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  };
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const token = localStorage.getItem("token");
+        // if (.id) {
+        //   const res = await axios.get(
+        //     `http://casting-ec2-1307338951.us-east-2.elb.amazonaws.com:7001/opportunities/`,
+        //     {
+        //       headers: {
+        //         Authorization: `Bearer ${token}`,
+        //       },
+        //     }
+        //   );
+        //   if (res) {
+        //     setMyRoles(res.data.roles);
+        //   }
+        // }
+      } catch (error) {}
+    })();
+  }, []);
+
+  const handleCheckSwitch = async (id) => {
+    const token = localStorage.getItem("token");
+
+    try {
+      const updatedRoles = myRoles.map((role) => {
+        if (role.id === id) {
+          const newStatus = role.status === "opened" ? "closed" : "opened";
+          return { ...role, status: newStatus };
+        }
+        return role;
+      });
+
+      setMyRoles(updatedRoles);
+
+      const res = await axios.put(
+        `http://casting-ec2-1307338951.us-east-2.elb.amazonaws.com:7001/opportunities/roles/${id}`,
+        { status: updatedRoles.find((role) => role.id === id)?.status },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleEdit = (id) => {
+    // .push(
+    //   `/creator/opportunities/${.id}/roles/${id}/edit/step-one`
+    // );
+  };
+
+  const handleDelete = async (id) => {
+    const token = localStorage.getItem("token");
+    try {
+      const res = await axios.delete(
+        `http://casting-ec2-1307338951.us-east-2.elb.amazonaws.com:7001/opportunities/roles/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      if (res) {
+        setMyRoles(myRoles.filter((item) => item.id !== id));
+      }
+    } catch (error) {}
+  };
+  return (
+    <div className="my-12">
+      <div className="flex flex-col sm:w-[500px] mx-auto gap-3">
+        <div className="flex sm:w-[500px] mx-auto justify-between items-center">
+          <p className="text-xl text-gray-500   ">Role(s) You're Casting</p>
+
+          <button
+            onClick={handleDynamicRoute}
+            className="hover:bg-blue-50 duration-200 text-gray-500 rounded-full text-3xl"
+          >
+            <IoAddOutline /> {""}
+          </button>
+        </div>
+
+        <div className="h-[200px] rounded-lg my-5 w-ful bg-white border-2 overflow-y-auto border-blue-100">
+          <p className="text-xl p-4">Role Name</p>
+
+          {myRoles.map((item, index) => (
+            <div
+              key={index}
+              className="border-b-[1px] border-blue-200 p-3 flex justify-between items-center"
+            >
+              <p className="text-xl">{item.name}</p>
+              <div className="flex items-center gap-1">
+                <div className="flex items-center ">
+                  <p className="text-xl">{item.status}</p>
+                  <Switch
+                    checked={item.status === "opened"}
+                    onChange={() => handleCheckSwitch(item.id)}
+                  />
+                </div>
+                <Tooltip title="Edit">
+                  <button
+                    onClick={() => handleEdit(item.id)}
+                    style={{ color: "#6371e0" }}
+                  >
+                    <ModeEditOutlineIcon /> {""}
+                  </button>
+                </Tooltip>
+
+                <Tooltip title="Delete">
+                  <button
+                    onClick={() => handleDelete(item.id)}
+                    style={{ color: "red" }}
+                  >
+                    <ClearIcon /> {""}
+                  </button>
+                </Tooltip>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="mb-10">
+          <label className="block text-xl mb-2" htmlFor="expirationDate">
+            ?When should this listing expire
+          </label>
+          {/* <DatePicker
+            minDate={currentDate}
+            label="Select a date"
+            value={expirationDate}
+            inputProps={{
+              className:
+                "w-full h-14 border-2 border-gray-300 outline-none rounded-xl text-lg border-none px-3",
+              id: "expirationDate",
+            }}
+            format="MM/dd/yyyy"
+            onChange={handleDateChange}
+            renderInput={(params) => <TextField {...params} />}
+            adapter={AdapterDateFns}
+          /> */}
+        </div>
+
+        <div className="flex items-center gap-4">
+          <button className="save" href={"/creator"}>
+            Save For Later
+          </button>
+
+          <> ︎ ︎ ︎</>
+          {/* {isLoading && expirationDate !== "" ? (
+            <div className="border-2 border-blue-700 bg-blue-700 rounded-md text-lg text-white px-4 py-1 font-semibold hover:bg-blue-600 duration-200">
+              <Loading buttonContent="Continue" />
+            </div>
+          ) : ( */}
+          <Button
+            onClick={handleSubmit}
+            variant="contained"
+            color="primary"
+            className="continuo"
+          >
+            {expirationDate ? (
+              <a href={`/creator/opportunities/edit/summary`}>Continue</a>
+            ) : (
+              "Continue"
+            )}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default StepTwoEdit;
